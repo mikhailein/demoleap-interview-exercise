2de5695a7d567467eac4145f0b35bd1c1dd6fc3f
Mihon <75487015+MikhailKolomoitsev@users.noreply.github.com>
2022-05-06 19:41:29 +0000

    failed func

--- js/index.js
+++ js/index.js
@@ -1,35 +1,59 @@
 
-const renderGraph = () => {
+const buttonGraph = document.querySelector("#buttonGraph");
+const buttonPie = document.querySelector("#buttonPie");
+const btnLocalRandomise = document.querySelector("#btnLocalRandomise");
+const btnGetServer = document.querySelector("#btnGetServer");
+const ratesChartWrapper = document.querySelector("#ratesChartWrapper");
+
+
+
+const dataRandomizer = (array) => {
+    array.forEach(i => {
+        i.y = Math.floor(Math.random() * 99)
+    }
+    )
+}
+
+const dataPointsGraph = [
+    { y: 13, label: "Jan." },
+    { y: 22, label: "Feb." },
+    { y: 23, label: "Mar." },
+    { y: 38, label: "Apr." },
+    { y: 45, label: "May." },
+    { y: 39, label: "Jun." },
+    { y: 42, label: "Jul." },
+    { y: 47, label: "Aug." },
+    { y: 34, label: "Sep." },
+    { y: 17, label: "Oct." },
+    { y: 28, label: "Nov." },
+    { y: 12, label: "Dec." },
+]
+
+const dataPointsPie = [
+    { y: 26, name: "School Aid", },
+    { y: 20, name: "Medical Aid" },
+    { y: 5, name: "Debt/Capital" },
+    { y: 3, name: "Elected Officials" },
+    { y: 7, name: "University" },
+    { y: 17, name: "Executive" },
+    { y: 22, name: "Other Local Assistance" }
+]
+
+const renderGraph = (data) => {
     const chart = new CanvasJS.Chart("chartContainer", {
         animationEnabled: true,
         theme: "light2", // "light1", "light2", "dark1", "dark2"
-        title: {
-            text: "Top Oil Reserves"
-        },
-        axisY: {
-            title: "Reserves(MMbbl)"
-        },
         data: [{
             type: "column",
             showInLegend: true,
             legendMarkerColor: "grey",
-            legendText: "MMbbl = one million barrels",
-            dataPoints: [
-                { y: 300878, label: "Venezuela" },
-                { y: 266455, label: "Saudi" },
-                { y: 169709, label: "Canada" },
-                { y: 158400, label: "Iran" },
-                { y: 142503, label: "Iraq" },
-                { y: 101500, label: "Kuwait" },
-                { y: 97800, label: "UAE" },
-                { y: 80000, label: "Russia" }
-            ]
+            dataPoints: data
         }]
     });
     chart.render();
 }
 
-const renderPie = () => {
+const renderPie = (data) => {
     function explodePie(e) {
         if (typeof (e.dataSeries.dataPoints[e.dataPointIndex].exploded) === "undefined" || !e.dataSeries.dataPoints[e.dataPointIndex].exploded) {
             e.dataSeries.dataPoints[e.dataPointIndex].exploded = true;
@@ -42,9 +66,6 @@ const renderPie = () => {
     const chart2 = new CanvasJS.Chart("chartContainerPie", {
         exportEnabled: true,
         animationEnabled: true,
-        title: {
-            text: "State Operating Funds"
-        },
         legend: {
             cursor: "pointer",
             itemclick: explodePie
@@ -54,37 +75,52 @@ const renderPie = () => {
             showInLegend: true,
             toolTipContent: "{name}: <strong>{y}%</strong>",
             indexLabel: "{name} - {y}%",
-            dataPoints: [
-                { y: 26, name: "School Aid", exploded: true },
-                { y: 20, name: "Medical Aid" },
-                { y: 5, name: "Debt/Capital" },
-                { y: 3, name: "Elected Officials" },
-                { y: 7, name: "University" },
-                { y: 17, name: "Executive" },
-                { y: 22, name: "Other Local Assistance" }
-            ]
+            dataPoints: data
         }]
     });
     chart2.render();
 }
 
-const buttonGraph = document.querySelector("#buttonGraph");
-const buttonPie = document.querySelector("#buttonPie");
-const ratesChartWrapper = document.querySelector("#ratesChartWrapper");
-
-const chartContainer = document.querySelector('#chartContainer')
-
-
-buttonGraph.addEventListener("click", () => {
-    console.log("Button buttonGraph was clicked");
+const renderFirst = () => {
     ratesChartWrapper.innerHTML = ''
     ratesChartWrapper.insertAdjacentHTML('beforeend', '<div id="chartContainer" style="height: 370px; width: 100%;"></div>')
-    renderGraph()
+    renderGraph(dataPointsGraph)
+}
+
+const renderSecond = () => {
+    ratesChartWrapper.innerHTML = ''
+    ratesChartWrapper.insertAdjacentHTML('beforeend', '<div id="chartContainerPie" style="height: 370px; width: 100%;"></div>')
+    renderPie(dataPointsPie)
+}
+
+async function getDataFromServer() {
+    try {
+        const data = await fetch('https://api.demoleap.com/exercise', {
+            method: "GET",
+            mode: 'no-cors',
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        }).then(res => console.log(res))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+buttonGraph.addEventListener("click", () => {
+    renderFirst()
 });
 
 buttonPie.addEventListener('click', () => {
-    console.log("Button buttonPie was clicked");
-    ratesChartWrapper.innerHTML = ''
-    ratesChartWrapper.insertAdjacentHTML('beforeend', '<div id="chartContainerPie" style="height: 370px; width: 100%;"></div>')
-    renderPie()
+    renderSecond()
+})
+
+btnLocalRandomise.addEventListener('click', () => {
+    dataRandomizer(dataPointsGraph)
+    dataRandomizer(dataPointsPie)
+    renderFirst()
+})
+
+btnGetServer.addEventListener('click', () => {
+    getDataFromServer()
 })
